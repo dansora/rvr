859b51a46e36ca82badea9677ce22165da233abe
Dan Sora <dannysora@gmail.com>
2024-04-22 12:19:19 +0100

    first commit

---
+++ src/java/nav-script.js
@@ -0,0 +1,6 @@
+// const toggleBtn = document.querySelector('.navToggle');
+// const body = document.body;
+
+// toggleBtn.addEventListener('click', _ => {
+//     body.classList.toggle('offScreenOpen');
+// })
